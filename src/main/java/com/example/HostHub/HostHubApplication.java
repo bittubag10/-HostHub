605b49352b451f80c6cee750d7c388da259b08a9
bittubag10 <bagbittu10@gmail.com>
2026-04-23 08:28:30 +0530

    Initial commit: HostHub Backend Setup with JPA

---
+++ src/main/java/com/example/HostHub/HostHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.HostHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HostHub/HostHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HostHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
