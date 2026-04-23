c8976fb96705107ea27165ffef84817ae593f5cc
bittubag10 <bagbittu10@gmail.com>
2026-04-23 08:47:13 +0530

    Added Hotel entity

---
+++ src/main/java/com/example/HostHub/entity/Hotel.java
@@ -0,0 +1,40 @@
+package com.example.HostHub.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photo;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
